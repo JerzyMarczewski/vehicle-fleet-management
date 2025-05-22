c78babbe733da09191da3ef20f53d5c3788b0a3e
Jerzy Marczewski <marczewski.jerzy@outlook.com>
2025-05-22 20:25:36 +0200

    add User entity

---
+++ src/main/java/com/fleetmanager/vehiclefleetmanagement/entity/User.java
@@ -0,0 +1,33 @@
+package com.fleetmanager.vehiclefleetmanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.*;
+
+@Entity
+@Table(name = "app_user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+}
+
+enum UserRole {
+    ADMIN,
+    OPERATOR
+}
