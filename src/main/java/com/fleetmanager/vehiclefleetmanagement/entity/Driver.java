b2446d70175fb8ace51de086107798a9328243c7
Jerzy Marczewski <marczewski.jerzy@outlook.com>
2025-05-22 20:25:05 +0200

    add Driver entity

---
+++ src/main/java/com/fleetmanager/vehiclefleetmanagement/entity/Driver.java
@@ -0,0 +1,37 @@
+package com.fleetmanager.vehiclefleetmanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.*;
+
+@Entity
+@Table(name = "driver")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Driver {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "license_no", unique = true, nullable = false)
+    private String licenseNo;
+
+    @Column(name = "employment_date")
+    private Date employmentDate;
+
+    @OneToMany(mappedBy = "driver", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Assignment> assignments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "driver", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Trip> trips = new ArrayList<>();
+}
