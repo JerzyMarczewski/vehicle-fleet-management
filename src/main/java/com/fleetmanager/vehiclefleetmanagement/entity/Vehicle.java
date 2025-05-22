658a1f5301d94ce226322b26a0ec4d902f29f7d3
Jerzy Marczewski <marczewski.jerzy@outlook.com>
2025-05-22 20:25:42 +0200

    add Vehicle entity

---
+++ src/main/java/com/fleetmanager/vehiclefleetmanagement/entity/Vehicle.java
@@ -0,0 +1,57 @@
+package com.fleetmanager.vehiclefleetmanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.*;
+
+@Entity
+@Table(name = "vehicle")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "registration_no", nullable = false, unique = true)
+    private String registrationNo;
+
+    @Column(unique = true)
+    private String vin;
+
+    private String brand;
+    private String model;
+
+    @Column(name = "production_year")
+    private Integer productionYear;
+
+    private Integer mileage;
+
+    @Enumerated(EnumType.STRING)
+    private VehicleStatus status;
+
+    @OneToMany(mappedBy = "vehicle", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Assignment> assignments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "vehicle", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Trip> trips = new ArrayList<>();
+
+    @OneToMany(mappedBy = "vehicle", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Maintenance> maintenances = new ArrayList<>();
+
+    @OneToMany(mappedBy = "vehicle", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Insurance> insurances = new ArrayList<>();
+
+    @OneToMany(mappedBy = "vehicle", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<FuelLog> fuelLogs = new ArrayList<>();
+}
+
+enum VehicleStatus {
+    AVAILABLE,
+    SERVICE,
+    OTHER
+}
