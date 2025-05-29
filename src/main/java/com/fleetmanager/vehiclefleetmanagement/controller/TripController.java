da3a6b4e0bb54ff8d9acb9f8eea8df854141184c
xAdiro <adiro.rostek@gmail.com>
2025-05-29 02:40:03 +0200

    Added some structure of TripController and index method

---
+++ src/main/java/com/fleetmanager/vehiclefleetmanagement/controller/TripController.java
@@ -0,0 +1,60 @@
+package com.fleetmanager.vehiclefleetmanagement.controller;
+
+import com.fleetmanager.vehiclefleetmanagement.DTO.trip.TripSummaryDTO;
+import com.fleetmanager.vehiclefleetmanagement.Mapper.TripMapper;
+import com.fleetmanager.vehiclefleetmanagement.service.TripService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+import static java.util.stream.Collectors.toList;
+
+@Controller
+@RequestMapping("/trip")
+public class TripController {
+    private final TripService tripService;
+    private final TripMapper tripMapper;
+
+
+    public TripController(TripService tripService, TripMapper tripMapper){
+        this.tripService = tripService;
+        this.tripMapper = tripMapper;
+    }
+
+    @GetMapping
+    public String showAllTrips(Model model){
+        model.addAttribute("trips",
+            tripService.getAllTrips()
+                .stream()
+                .map(tripMapper::toSummaryDTO)
+                .collect(toList()));
+       return "trip/index";
+    }
+
+    @GetMapping("/{id}")
+    public TripSummaryDTO showTripDetails(@PathVariable("id") UUID id){
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public TripSummaryDTO updateTrip(@RequestBody TripSummaryDTO trip, @PathVariable String id){
+        return null;
+    }
+
+    @DeleteMapping
+    public TripSummaryDTO deleteTrip(@RequestBody UUID id){
+        return null;
+    }
+
+    @GetMapping("/new")
+    public TripSummaryDTO showNewTripForm(){
+        return null;
+    }
+
+    @PostMapping("/new")
+    public TripSummaryDTO createNewTrip(@RequestBody TripSummaryDTO trip){
+        return null;
+    }
+}
