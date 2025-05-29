69362d34c3dedff1dec71f2b462dee7c0ad9ff1d
xAdiro <adiro.rostek@gmail.com>
2025-05-29 02:38:39 +0200

    Bare minimum of TripService

---
+++ src/main/java/com/fleetmanager/vehiclefleetmanagement/service/TripService.java
@@ -0,0 +1,12 @@
+package com.fleetmanager.vehiclefleetmanagement.service;
+
+import com.fleetmanager.vehiclefleetmanagement.entity.Trip;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface TripService {
+
+    List<Trip> getAllTrips();
+}
---
+++ src/main/java/com/fleetmanager/vehiclefleetmanagement/service/TripServiceImpl.java
@@ -0,0 +1,21 @@
+package com.fleetmanager.vehiclefleetmanagement.service;
+
+import com.fleetmanager.vehiclefleetmanagement.entity.Trip;
+import com.fleetmanager.vehiclefleetmanagement.repository.TripRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TripServiceImpl implements TripService{
+    private TripRepository tripRepository;
+
+    public TripServiceImpl(TripRepository tripRepository){
+        this.tripRepository = tripRepository;
+    }
+
+    @Override
+    public List<Trip> getAllTrips() {
+        return List.of();
+    }
+}
