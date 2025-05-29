d66d6ffe08acfb74652cbc9470b9200e6e5ab692
xAdiro <adiro.rostek@gmail.com>
2025-05-29 02:39:19 +0200

    DTO and mapper for trip summary

---
+++ src/main/java/com/fleetmanager/vehiclefleetmanagement/Mapper/TripMapper.java
@@ -0,0 +1,19 @@
+package com.fleetmanager.vehiclefleetmanagement.Mapper;
+
+import com.fleetmanager.vehiclefleetmanagement.DTO.trip.TripSummaryDTO;
+import com.fleetmanager.vehiclefleetmanagement.entity.Trip;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TripMapper {
+    public TripSummaryDTO toSummaryDTO(Trip trip){
+        return new TripSummaryDTO(
+                trip.getId(),
+                trip.getStartTime(),
+                trip.getEndTime(),
+                trip.getStartLocation(),
+                trip.getEndLocation(),
+                trip.getDistanceKm()
+        );
+    }
+}
