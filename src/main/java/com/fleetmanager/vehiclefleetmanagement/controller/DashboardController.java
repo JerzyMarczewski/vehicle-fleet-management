5f2085404cd0735528a7234a78604343cd43e5b9
Jerzy Marczewski <marczewski.jerzy@outlook.com>
2025-06-05 23:39:34 +0200

    add DashboardController with root mapping

---
+++ src/main/java/com/fleetmanager/vehiclefleetmanagement/controller/DashboardController.java
@@ -0,0 +1,15 @@
+package com.fleetmanager.vehiclefleetmanagement.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class DashboardController {
+
+    @GetMapping("/")
+    public String showDashboard() {
+        return "dashboard";
+    }
+}
