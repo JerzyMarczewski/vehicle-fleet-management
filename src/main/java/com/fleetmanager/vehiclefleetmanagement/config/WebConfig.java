5c7d6c59e0b364bb1ee2f6b1d3abace7f9cf9005
Jerzy Marczewski <marczewski.jerzy@outlook.com>
2025-06-06 21:52:46 +0200

    add messageSource bean to support localized messages

--- src/main/java/com/fleetmanager/vehiclefleetmanagement/config/WebConfig.java
+++ src/main/java/com/fleetmanager/vehiclefleetmanagement/config/WebConfig.java
@@ -1,14 +1,29 @@
 package com.fleetmanager.vehiclefleetmanagement.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    @Primary
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setUseCodeAsDefaultMessage(true);
+        messageSource.setCacheSeconds(10);
+        return messageSource;
+    }
+
+
     @Bean
     public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
         return new HiddenHttpMethodFilter();
