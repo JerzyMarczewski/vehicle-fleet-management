639b3d727ce540bf31485e4b706d63dd884b9e4e
Jerzy Marczewski <marczewski.jerzy@outlook.com>
2025-06-06 21:53:44 +0200

    update VehicleService to use new DTOs and enum types

--- src/main/java/com/fleetmanager/vehiclefleetmanagement/service/VehicleService.java
+++ src/main/java/com/fleetmanager/vehiclefleetmanagement/service/VehicleService.java
@@ -1,16 +1,17 @@
 package com.fleetmanager.vehiclefleetmanagement.service;
 
-import com.fleetmanager.vehiclefleetmanagement.dto.CreateVehicleRequestDTO;
+import com.fleetmanager.vehiclefleetmanagement.dto.AddVehicleRequestDTO;
 import com.fleetmanager.vehiclefleetmanagement.dto.EditVehicleRequestDTO;
 import com.fleetmanager.vehiclefleetmanagement.entity.Vehicle;
+import com.fleetmanager.vehiclefleetmanagement.enums.VehicleSearchType;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface VehicleService {
-    Optional<Vehicle> searchByType(String type, String query);
-    Vehicle createVehicle(CreateVehicleRequestDTO createVehicleRequestDTO);
+    Optional<Vehicle> searchByType(VehicleSearchType type, String query);
+    Vehicle addVehicle(AddVehicleRequestDTO addVehicleRequestDTO);
     Vehicle editVehicle(UUID id, EditVehicleRequestDTO editVehicleRequestDTO);
     Vehicle deleteVehicle(UUID id);
     List<Vehicle> getAllVehicles();
