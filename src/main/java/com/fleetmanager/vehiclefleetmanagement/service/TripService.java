d96bd0e3d79751b7db05bdd26c9e1bb77b97403d
xAdiro <adiro.rostek@gmail.com>
2025-06-09 02:42:08 +0200

    Added rest of needed trip service methods

--- src/main/java/com/fleetmanager/vehiclefleetmanagement/service/TripService.java
+++ src/main/java/com/fleetmanager/vehiclefleetmanagement/service/TripService.java
@@ -1,13 +1,26 @@
 package com.fleetmanager.vehiclefleetmanagement.service;
 
+import com.fleetmanager.vehiclefleetmanagement.DTO.trip.TripDetailsDTO;
+import com.fleetmanager.vehiclefleetmanagement.DTO.trip.TripSummaryDTO;
 import com.fleetmanager.vehiclefleetmanagement.entity.Trip;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public interface TripService {
+    Trip createTrip(TripDetailsDTO trip);
 
     List<Trip> getAllTrips();
-    Trip createTrip(Trip trip);
+    List<TripSummaryDTO> getAllTripSummaryDTOs();
+    List<TripDetailsDTO> getAllTripDetailsDTOs();
+
+    Optional<Trip> getTripById(UUID id);
+    Optional<TripDetailsDTO> getTripDetailsDTOById(UUID id);
+
+    Trip editTrip(UUID id, TripDetailsDTO changedTrip);
+
+    UUID deleteTripById(UUID id);
 }
--- src/main/java/com/fleetmanager/vehiclefleetmanagement/service/TripServiceImpl.java
+++ src/main/java/com/fleetmanager/vehiclefleetmanagement/service/TripServiceImpl.java
@@ -1,17 +1,31 @@
 package com.fleetmanager.vehiclefleetmanagement.service;
 
+import com.fleetmanager.vehiclefleetmanagement.DTO.trip.TripDetailsDTO;
+import com.fleetmanager.vehiclefleetmanagement.DTO.trip.TripSummaryDTO;
 import com.fleetmanager.vehiclefleetmanagement.entity.Trip;
+import com.fleetmanager.vehiclefleetmanagement.mapper.TripMapper;
 import com.fleetmanager.vehiclefleetmanagement.repository.TripRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 public class TripServiceImpl implements TripService{
-    private TripRepository tripRepository;
+    private final TripRepository tripRepository;
+    private final TripMapper tripMapper;
 
-    public TripServiceImpl(TripRepository tripRepository){
+    public TripServiceImpl(TripRepository tripRepository, TripMapper tripMapper){
         this.tripRepository = tripRepository;
+        this.tripMapper = tripMapper;
+    }
+
+    @Override
+    public Trip createTrip(TripDetailsDTO trip) {
+        return tripRepository.save(tripMapper.fromDetailsDTO(trip));
     }
 
     @Override
@@ -20,7 +34,47 @@ public class TripServiceImpl implements TripService{
     }
 
     @Override
-    public Trip createTrip(Trip trip) {
-        return tripRepository.save(trip);
+    public List<TripSummaryDTO> getAllTripSummaryDTOs() {
+        List<Trip> trips = getAllTrips();
+        return trips.stream()
+                .map(tripMapper::toSummaryDTO)
+                .collect(toList());
+    }
+
+    @Override
+    public List<TripDetailsDTO> getAllTripDetailsDTOs() {
+        var trips = getAllTrips();
+        return trips.stream()
+                .map(tripMapper::toDetailsDTO)
+                .collect(toList());
+    }
+
+    @Override
+    public Optional<Trip> getTripById(UUID id) {
+        return tripRepository.findById(id);
+    }
+
+    @Override
+    public Optional<TripDetailsDTO> getTripDetailsDTOById(UUID id) {
+        var trip = getTripById(id);
+        return tripMapper.toDetailsDTO(trip);
+    }
+
+    @Override
+    public UUID deleteTripById(UUID id) {
+        if (!tripRepository.existsById(id)){
+            throw new RuntimeException("Trip not found with ID: " + id);
+        }
+
+        tripRepository.deleteById(id);
+        return id;
+    }
+
+    @Override
+    public Trip editTrip(UUID id, TripDetailsDTO changedTrip) {
+         Trip trip = getTripById(id).orElseThrow(() -> new RuntimeException("Trip with ID: " + id + " does not exist"));
+         tripMapper.updateTripFromDetailsDTO(changedTrip, trip);
+
+         return tripRepository.save(trip);
     }
 }
